a30ed9c53c639405b025e81ef0ae52c8cbab00b4
EliasN021 <97088458+EliasN021@users.noreply.github.com>
2022-01-13 14:22:01 +0100

    new version with texture (optionnal)

--- generate_simdata.m
+++ generate_simdata.m
@@ -1,16 +1,19 @@
-function im = generate_simdata(K)
+function im = generate_simdata(K, texture_files)
 %
 % The function generates a simulated data image with black background and
 % the following randomly placed figures : a circle, a triangle and a
 % square.
 %
 % Usage: im = generate_simdata(K)
-% Input: K is the size of the image.
+% Inputs: K is the size of the image.
+%         texture_files (optional): a string, the path of a folder
+%         containing three texture images
 % Output: K*K sized black image with randomly placed figures.
 %
 % Date: 13/01/2022
 
 % Create the shapes in square matrices of size K/3
+
 k = floor(K/3);
 vector = [1:k];
 
@@ -25,13 +28,46 @@ triangle = triangle + repmat(k-vector',1,k);
 triangle(triangle > k) = 0;
 triangle(triangle > 0) = 1;
 
-% Generate random coordinates for the shapes, so that the entire shapes are
-% in the image
+switch nargin
+    case 1
+        square = ones(k);
+
+    case 2
+        if ~isfolder(texture_files)
+            error('second argument must be a correct folder path string.')
+        end
+
+        % apply the textures if a folder path is given
+        listing = dir(texture_files);
+        listing = listing(~[listing.isdir]);
+
+        texture = {};
+        texture{1} = readImage([texture_files '\' listing(1).name]);
+        texture{2} = readImage([texture_files '\' listing(2).name]);
+        texture{3} = readImage([texture_files '\' listing(3).name]);
+
+        texture{1} = imresize(texture{1},[k,k]);
+        texture{2} = imresize(texture{2},[k,k]);
+        texture{3} = imresize(texture{3},[k,k]);
+
+        square = texture{1};
+
+        texture{2}(triangle == 0) = 0;
+        triangle = texture{2};
+
+        texture{3}(circle == 0) = 0;
+        circle = texture{3};
+
+
+end
+
+% Generate random coordinates for the shapes, while making sure that the
+% entire shapes are in the image
 coords = floor(rand(3,2)*(K-k));
 
 % Return the image
 im = zeros(K);
-im(coords(1,1):coords(1,1)+k-1, coords(1,2):coords(1,2)+k-1) = 1;
+im(coords(1,1):coords(1,1)+k-1, coords(1,2):coords(1,2)+k-1) = square;
 im(coords(2,1):coords(2,1)+k-1, coords(2,2):coords(2,2)+k-1) = im(coords(2,1):coords(2,1)+k-1, coords(2,2):coords(2,2)+k-1) + triangle;
 im(coords(3,1):coords(3,1)+k-1, coords(3,2):coords(3,2)+k-1) = im(coords(3,1):coords(3,1)+k-1, coords(3,2):coords(3,2)+k-1) + circle;
 

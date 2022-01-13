defebcb0e040f5472c7f6ba7682607936f12f248
EliasN021 <97088458+EliasN021@users.noreply.github.com>
2022-01-13 15:21:36 +0100

    Add files via upload

---
+++ error_measure.m
@@ -0,0 +1,17 @@
+function err = error_measure(Data, Reconstruction)
+% Gives the error measure of the reconstructed matrix using the Frobenius
+% norm.
+
+% Usage: error_measure(Data, Reconstruction)
+% Inputs: Data, the original matrix.
+%         Reconstruction, the reconstructed matrix to compare with the
+%         original data.
+
+if ~all(size(Data) == size(Reconstruction))
+    error('Both matrices must be of same size')
+end
+
+Num = norm(Data - Reconstruction, 'fro');
+Den = norm(Data,'fro');
+
+err = Num/Den;

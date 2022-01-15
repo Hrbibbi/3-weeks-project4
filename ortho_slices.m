d5e44a425f257621166873e7ce51d6f539c77d63
EliasN021 <97088458+EliasN021@users.noreply.github.com>
2022-01-15 11:51:44 +0100

    comments

--- ortho_slices.m
+++ ortho_slices.m
@@ -1,14 +1,15 @@
 function [O1,O2,O3] = ortho_slices(vol_in,s1,s2,s3)
-% Returns three 2D orthoslices, from a 3D volume Matrix.
+% Returns three 2D orthoslices (planes in the three orthogonal orientations)
+% from a 3D volume Matrix.
 %
 % Usage: [O1,O2,O3] = ortho_slices(vol_in,s1,s2,s3);
 % Inputs: vol_in - the 3D matrix to take three slices from.
-%         s1 - first slice z-coordinate.
-%         s2 - second slice z-coordinate.
-%         s3 - third slice z-coordinate.
-% Outputs: O1 - first slice.
-%          O2 - second slice.
-%          O3 - third slice.
+%         s1 - xy-slice's z-coordinate.
+%         s2 - xz-slice's y-coordinate.
+%         s3 - yz-slice's x-coordinate.
+% Outputs: O1 - xy-slice.
+%          O2 - xz-slice.
+%          O3 - yz-slice.
 % ! Remark: the first character of the variable is the capital letter 'o',
 % not to be confused with the digit 0.
 %

1bf25d42b95ad3eb199ece6b012ec20dcfe4f19a
Lachrynier <97088473+Lachrynier@users.noreply.github.com>
2022-01-15 17:11:23 +0100

    Add files via upload

--- addnoise.m
+++ addnoise.m
@@ -1,34 +1,33 @@
 function im_noisy = addnoise(im,tau,varargin)
 % Adds noise to either a normal or DFT image.
-% Input arguments:
+% --- Input arguments ---
 % im: 2D array image
 % pc: percentage either as a decimal (default) or in [0,100].
 % varargin{1} (optional argument): specify 'p' if tau is in [0,100].
+% --- Output arguments ---
+% im_noisy: 2D array of image with noise
 
+% check to see if fraction is specified as decimal or percentage value
 if nargin == 3 && varargin{1} == 'p'
+    % if so, convert to decimal
     tau = tau/100;
 end
-
-% This is for the non-complex case
-noise_i = zeros(size(im));
-v_r = 1;
-e_c = 0;
-
+% in case image is not already double, i.e. uint8
 im = double(im);
 
-% Frobenius norm of input
-fro_im = norm(im,'fro');
-
-% If complex
-if ~isreal(im)
-    v_r = 1/sqrt(2);
-    v_c = 1/sqrt(2);
-    r_c = rand(size(im));
-    r_c = r_c/norm(r_c,'fro');
-    e_c = v_c*tau*r_c*fro_im;
+if any(imag(im) ~= 0)
+    % complex
+    r = rand(size(im));
+    r = r/norm(r,'fro');
+    e_imag = r*norm(imag(im),'fro');
+    r = rand(size(im));
+    r = r/norm(r,'fro');
+    e_real = r*norm(real(im),'fro');
+    im_noisy = im + tau*(e_real + e_imag*1i);
+else
+    % real
+    r = rand(size(im));
+    r = r/norm(r,'fro');
+    e = tau*r*norm(im,'fro');
+    im_noisy = im + e;
 end
-
-r_r = rand(size(im));
-r_r = r_r/norm(r_r,'fro');
-e_r = v_r*tau*r_r*fro_im;
-im_noisy = im + e_r + e_c;

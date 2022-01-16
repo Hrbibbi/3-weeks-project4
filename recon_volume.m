d7bdbf2c903ed3c41298785b92b6034ea7719e38
EliasN021 <97088458+EliasN021@users.noreply.github.com>
2022-01-16 14:19:25 +0100

    comments corrections

--- recon_volume.m
+++ recon_volume.m
@@ -1,11 +1,10 @@
 function vol_out = recon_volume(vol_in,slice_vec)
-% recon_volume reconstruct a subset of slices from a volume scan.
+% recon_volume performs inverse DFT and reconstructs a subset of slices from a volume scan.
 % --- Input arguments ---
 % vol_in: 3D array of the composed volume
 % slice_vec: vector that recounts which slices to be reconstructed
 % --- Output arguments ---
-% vol_out: 3D array of the same size as vol_in, but containing the
-%          reconstructed slices
+% vol_out: 3D array of the reconstructed slices
 % Date: 14/01/2022
 
 % Checks if all elements of slice_vec is contained in the 3rd dimension of vol_in

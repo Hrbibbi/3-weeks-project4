04ca38bcf7d0451eb97f1b8bf5df15894f1423f3
EliasN021 <97088458+EliasN021@users.noreply.github.com>
2022-01-17 13:08:23 +0100

    fokortede

--- signal_limited.m
+++ signal_limited.m
@@ -1,49 +1,31 @@
-function lim_signal =  signal_limited(signal,frac,options)
+function lim_signal =  signal_limited(signal,frac)
 % Imitates the sampling of a DFT by zero-padding a part of the information.
 %
 % Inputs: signal - a 2D DFT matrix, the data to work on.
 %         frac - a floating number from 0 to 1, the ratio of the data to
 %                keep. (the rest is deleted)
-%         options (optionnal) - 'middle' (default setting) or 'random'.
 % Output: A matrix containing a fraction of the data from the input signal,
 % but of the same size.
-%
 % Date: 01-2022
 
 % Verify the inputs types 
 arguments
     signal double
     frac double
-    options char = 'middle'
 end
 
+lim_signal = zeros(size(signal));
 
-switch options
-    case 'middle'
-        lim_signal = zeros(size(signal));
+% Perform FFT on the input signal 
+signal = fftshift(signal);
 
-        % Perform FFT on the input signal 
-        signal = fftshift(signal);
-        
-        % C is the coordinate of the center
-        C = round(length(signal)/2);
-        length(signal); % <------------ This one
+% C is the coordinate of the center
+C = round(length(signal)/2);
 
-        % L is the half length of the preserved data square
-        L = floor((length(signal)*sqrt(frac))/2);
-        lim_signal(C-L:C+L,C-L:C+L) = signal(C-L:C+L,C-L:C+L);
+% L is the half length of the preserved data square
+L = floor((length(signal)*sqrt(frac))/2);
+lim_signal(C-L:C+L,C-L:C+L) = signal(C-L:C+L,C-L:C+L);
 
-        % Invert FFT back
-        lim_signal = fftshift(lim_signal);
-
-    case 'random'
-        lim_signal = zeros(size(signal));
-        
-        % Randomly select data
-        K = rand(size(signal));
-        i = find(K >= frac);
-        lim_signal(i) = signal(i);
-
-end
-
+% Invert FFT back
+lim_signal = fftshift(lim_signal);
 end

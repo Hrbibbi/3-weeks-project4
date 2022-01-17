6513220504dc635ffaac85a90c24dde6e8e78d00
EliasN021 <97088458+EliasN021@users.noreply.github.com>
2022-01-17 10:04:18 +0100

    Update readImage.m

--- readImage.m
+++ readImage.m
@@ -1,2 +1,12 @@
 function im = readImage(fileName)
-im = 1/255*double(imread(fileName));
+% Loads in image in a matrix form with values from 0 to 1
+%
+% input : fileName, name of the image file.
+% output : the image in matrix-form
+%
+% Date = 01-2022
+
+im = imread(fileName);
+if any(im > 1)
+    im = 1/255*double();
+end

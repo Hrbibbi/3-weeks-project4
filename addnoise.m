b6cba7bb0c77fe712ec454ae52265d41c65d4edb
Lachrynier <97088473+Lachrynier@users.noreply.github.com>
2022-01-18 11:23:54 +0100

    Update addnoise.m

--- addnoise.m
+++ addnoise.m
@@ -6,14 +6,20 @@ function im_noisy = addnoise(im,tau,varargin)
 % varargin{1} (optional argument): specify 'p' if tau is in [0,100].
 % --- Output arguments ---
 % im_noisy: 2D array of image with noise
+% Date: 01-2022
 
 % check to see if fraction is specified as decimal or percentage value
 if nargin == 3 && varargin{1} == 'p'
-    % if so, convert to decimal
-    tau = tau/100;
+    if tau < 0 || tau > 100
+        error('Invalid percentage')
+    end
+    tau = tau/100; % convert to decimal
+else
+    if tau < 0 || tau > 1
+        error('Invalid decimal percentage')
+    end
 end
-% in case image is not already double, i.e. uint8
-im = double(im);
+im = double(im); % in case image is not already double, i.e. uint8
 
 if any(imag(im) ~= 0)
     % complex
